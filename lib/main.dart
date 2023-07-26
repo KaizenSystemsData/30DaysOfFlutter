1e5e6a9bb8bb929127de51ef294424ed73fd08c6
KaizenSystemsData <kaizensystemsdata@gmail.com>
2023-07-26 16:33:29 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/homepage.dart';
-import 'package:flutter_catalog/homepage.dart';
+import 'package:flutter_catalog/pages/homepage.dart';
+import 'package:flutter_catalog/pages/homepage.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
